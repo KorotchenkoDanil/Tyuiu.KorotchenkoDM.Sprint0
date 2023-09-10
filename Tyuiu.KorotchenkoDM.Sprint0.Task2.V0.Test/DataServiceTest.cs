bab412c252505fe67bd2208fe64cfd98128df49f
Tyuiu.KorotchenkoDM@outlook.com <danil.korotchenko.2005@gmail.com>
2023-09-10 13:44:47 +0500

    Task2

---
+++ Tyuiu.KorotchenkoDM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KorotchenkoDM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KorotchenkoDM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Данил";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Данил", res);
+        }
+    }
+}
